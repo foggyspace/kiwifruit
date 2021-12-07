e66b84420edab8777714e81d8cc7e1eccb0bea97
seaung <seaung.c0m7de@protonmail.com>
2021-12-08 00:02:18 +0800

    rebuild project

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=manager.py
+FLASK_ENV=development
--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,6 @@
 from flask import Flask
-from app.extensions import db, login_manager, migrate
-from app.configs import config
 
 
-def create_app(config_name=None):
-    if config_name is None:
-        config_name = 'development'
+def create_app():
     app = Flask(__name__)
-    app.config.from_object(config[config_name])
-    register_blueprints(app)
-    register_extensions(app)
     return app
-
-
-def register_blueprints(app):
-    from app.blueprints.admin import admin
-    from app.blueprints.auth import auth_bp
-    app.register_blueprint(admin)
-    app.register_blueprint(auth_bp)
-
-
-def register_extensions(app):
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
-    login_manager.init_app(app)
-    migrate.init_app(app, db)
-
