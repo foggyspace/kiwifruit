7b24bf4a1face2f9a229f8f099c183015ec875fb
seaung <seaung.c0m7de@protonmail.com>
2024-07-15 21:46:17 +0800

    :ok_hand: feat:添加组件

---
+++ kiwifruit/src/views/login/components/LoginForm.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Button, Form, Input, message } from "antd";
+import { useNavigate } from "react-router-dom";
+
+
+const LoginFormComponent = () => {
+    return (
+        <Form></Form>
+    );
+};
+
+export default LoginFormComponent;
+
---
+++ kiwifruit/src/views/login/index.tsx
@@ -0,0 +1,7 @@
+const Login = () => {
+    return (
+        <div className='login-container'></div>
+    );
+};
+
+export default Login;
